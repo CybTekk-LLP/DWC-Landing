db967a78d9dec91329df3880004563f1fdf9b27b
Gourav Saini <gouravsaini9797@gmail.com>
2024-11-05 11:07:44 +0530

    feat: about-slideshow

--- js/script.js
+++ js/script.js
@@ -4,3 +4,4 @@ import { HeadingComponent } from "./heading-component.mjs";
 import { AnimatedCard } from "./animated-card.mjs";
 import { InfrastructureComponent } from "./infrastructure-component.mjs";
 import { DwcProjectCard } from "./project-card-component.mjs";
+import { AboutSlideShow } from "./about-slideshow.mjs";
