fe62bdf04ed3b5fff73b5d8c8ea57898a99914e0
yashrajbharti <y.bharti@cybtekk.com>
2024-11-07 13:50:20 +0530

    add: navbar component

---
+++ js/navbar.mjs
@@ -0,0 +1,422 @@
+
+import "https://unpkg.com/monotone-shapes@1.0.5/src/app.js"
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+   
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+ul {
+    list-style-type: none;
+}
+
+header {
+    position: fixed;
+    text-align: center;
+    margin: auto;
+    inline-size: 100%;
+}
+
+header img {
+    display: inline-block;
+    position: absolute;
+    inset-inline-start: 0;
+    inset-block-start: 0;
+    margin: 20px;
+    z-index: 1;
+}
+
+nav {
+    position: relative;
+    display: inline-block;
+    font-size: 12px;
+    margin-block-start: 20px;
+    background-color: #fafffda9;
+    backdrop-filter: blur(20px);
+    border-radius: 15px;
+    padding: 20px;
+    color: #525253;
+    text-decoration: none;
+    transition: all 0.3s ease;
+    block-size: 50px;
+    overflow: hidden;
+}
+
+ul {
+    display: flex;
+    gap: 20px;
+}
+
+li {
+    cursor: pointer;
+}
+
+ul>li>ul {
+    inset-inline-start: 0;
+    position: absolute;
+    display: grid;
+    text-align: start;
+    inline-size: 100%;
+    block-size: 100px;
+    justify-content: center;
+    align-items: center;
+    grid-template-columns: 1fr 1fr 1fr;
+    margin-block-start: 20px;
+    font-size: 16px;
+    padding-inline-start: 20px;
+
+}
+
+nav:has(.hover):hover {
+    block-size: 180px;
+}
+
+li>span {
+    position: relative;
+    display: inline-block;
+    transition: all 0.3s ease;
+}
+
+span>a {
+    color: #565656;
+    text-decoration: none;
+}
+
+span>a:hover {
+    color: #2C2C2D;
+}
+
+li>span::after {
+    position: absolute;
+    content: "";
+    inline-size: 2px;
+    block-size: 2px;
+    background-color: #525253;
+    inset-inline-start: 50%;
+    translate: -50%;
+    inset-block-end: -5px;
+    transition: all 0.3s ease;
+    opacity: 0;
+}
+
+li>span:hover::after,
+li>span.hover::after {
+    scale: 10 1;
+    opacity: 1;
+}
+
+nav:not(:hover) span.hover::after {
+    opacity: 0;
+}
+
+li>span:hover {
+    color: #2C2C2D;
+}
+
+nav>ul>li>ul>li {
+    display: none;
+    z-index: 0;
+
+}
+
+nav>ul>li>ul>li>a {
+    display: flex;
+    gap: 5px;
+    align-items: center;
+    text-decoration: none;
+    color: #565656;
+    text-underline-offset: 3px;
+    transition: all 0.3s ease;
+}
+
+nav>ul>li>ul>li>a:hover {
+    text-decoration: underline;
+    text-underline-offset: 3px;
+    text-decoration-color: #565656;
+    color: #2C2C2D;
+}
+
+
+nav>ul>li:has(span.hover)>ul>li {
+    display: block;
+    z-index: 1;
+}
+
+
+ul>li>ul>li {
+    color: #565656;
+    transition: all 0.3s ease;
+}
+
+ul>li>ul>li:hover {
+    color: #2C2C2D;
+}
+
+label {
+    position: absolute;
+    display: none;
+}
+
+@media screen and (width < 868px) {
+    header {
+        position: fixed;
+        block-size: 100dvh;
+    }
+
+    nav {
+        position: relative;
+        display: block;
+        font-size: 12px;
+        block-size: 100dvh;
+        background-color: #fafffda9;
+        backdrop-filter: blur(20px);
+        border-radius: 15px;
+        padding: 20px;
+        margin: 0;
+        color: #525253;
+        text-decoration: none;
+        transition: all 0.3s ease;
+        overflow: hidden;
+        padding-block-start: 80px;
+    }
+
+    nav:hover,
+    nav:has(.hover):hover {
+        block-size: 100dvh;
+    }
+
+    nav>ul {
+        display: flex;
+        flex-direction: column;
+    }
+
+    nav>ul>li:has(span.hover)>ul {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        position: relative;
+        z-index: 1;
+        block-size: 100%;
+        padding: 0;
+        background-color: #fafffda9;
+        padding: 10px;
+        overflow: hidden;
+        transition: all 0.3s ease;
+    }
+
+    header nav {
+        display: none;
+    }
+
+    label {
+        display: block;
+        inset-inline-end: 0;
+        inset-block-start: 0;
+        margin: 20px;
+
+    }
+
+    label input {
+        display: none;
+    }
+
+    label span {
+        display: block;
+        margin: 12px;
+        block-size: 2px;
+        inline-size: 30px;
+        background-color: #2C2C2D;
+        transition: all 0.3s ease;
+    }
+
+    label input:checked~span:first-of-type {
+        rotate: 45deg;
+        translate: 0 6px;
+    }
+
+    label input:checked~span:last-of-type {
+        rotate: -45deg;
+        translate: 0 -6px;
+    }
+
+    header:has(label>input:checked) nav {
+        display: block;
+    }
+}
+  </style>
+
+  <header>
+        <a href="./#"><img src="./icons/Logo.svg" alt=""></a>
+        <nav>
+            <ul>
+                <li>
+                    <span><a href="./about-company.html">About</a></span>
+                    <ul>
+                        <li><a href="./about-company.html"><monotone-shape shape-id=""
+                                    text="Company"></monotone-shape>Company</a></li>
+                        <li><a href="./why-choose-us.html"><monotone-shape shape-id=""
+                                    text="Why Choose Us"></monotone-shape>Why Choose Us</a></li>
+                        <li><a href="./ethos.html"><monotone-shape shape-id="" text="Ethos"></monotone-shape>Ethos</a>
+                        </li>
+                        <li><a href="./leadership.html"><monotone-shape shape-id=""
+                                    text="Leadership"></monotone-shape>Leadership</a></li>
+                        <li><a href="./csr.html"><monotone-shape shape-id="" text="CSR"></monotone-shape>CSR</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <span><a href="./services.html">Services</a></span>
+                    <ul>
+                        <li><a href="./services.html#infrastructure-development"><monotone-shape shape-id=""
+                                    text="Infrastructure Development"></monotone-shape>Infrastructure Development</a>
+                        </li>
+                        <li><a href="./services.html#design-build"><monotone-shape shape-id=""
+                                    text="Design & Build"></monotone-shape>Design & Build</a></li>
+                        <li><a href="./services.html#hospitality"><monotone-shape shape-id=""
+                                    text="Hospitality"></monotone-shape>Hospitality</a></li>
+                        <li><a href="./services.html#joint-venture-model"><monotone-shape shape-id=""
+                                    text="Joint Venture Model"></monotone-shape>Joint Venture Model</a></li>
+                    </ul>
+
+                </li>
+                <li>
+                    <span><a href="./industries.html">Industries</a></span>
+                    <ul>
+                        <li><a href="./industries.html#commercial"><monotone-shape shape-id=""
+                                    text="Commercials"></monotone-shape>Commercial</a></li>
+                        <li><a href="./industries.html#residential"><monotone-shape shape-id=""
+                                    text="Residential"></monotone-shape>Residential</a></li>
+                        <li><a href="./industries.html#hospitality-sector"><monotone-shape shape-id=""
+                                    text="Hospitality Sector"></monotone-shape>Hospitality Sector</a></li>
+                        <li><a href="./industries.html#education-sector"><monotone-shape shape-id=""
+                                    text="Education Sector"></monotone-shape>Education Sector</a></li>
+                        <li><a href="./industries.html#hospitals"><monotone-shape shape-id=""
+                                    text="Hospitals"></monotone-shape>Hospitals</a></li>
+                        <li><a href="./industries.html#office-spaces"><monotone-shape shape-id=""
+                                    text="Office Spaces"></monotone-shape>Office Spaces</a></li>
+                        <li><a href="./industries.html#it-sector"><monotone-shape shape-id=""
+                                    text="IT Sector"></monotone-shape>IT Sector</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <span><a href="./clients.html">Clients</a></span>
+                    <ul>
+                        <li><a href="./clients.html#mohali-citi-centre"><monotone-shape shape-id=""
+                                    text="Mohali Citi Centre"></monotone-shape>Mohali Citi Centre</a></li>
+                        <li><a href="./clients.html#marbella-grand"><monotone-shape shape-id=""
+                                    text="Marbella Grand"></monotone-shape>Marbella Grand</a></li>
+                        <li><a href="./clients.html#prisma-plaza"><monotone-shape shape-id=""
+                                    text="Prisma Plaze"></monotone-shape>Prisma Plaze</a></li>
+                        <li><a href="./clients.html#sps-realtors"><monotone-shape shape-id=""
+                                    text="SPS Realtors"></monotone-shape>SPS Realtors</a></li>
+                        <li><a href="./clients.html#vrs"><monotone-shape shape-id="" text="VRS"></monotone-shape>VRS</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <span><a href="./#projects">Projects</a></span>
+                    <ul>
+                        <li><a href="./sps-helios.html"><monotone-shape shape-id=""
+                                    text="SPS Helios"></monotone-shape>SPS Helios</a></li>
+                        <li><a href="./mohali-citi-centre.html"><monotone-shape shape-id=""
+                                    text="Mohali Citi Centre"></monotone-shape>Mohali Citi Centre</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <span><a href="./careers.html">Careers</a></span>
+                    <ul>
+                        <li><a href="./careers.html#civil-engineer"><monotone-shape shape-id=""
+                                    text="Civil Engineer"></monotone-shape>Civil Engineer</a></li>
+                        <li><a href="./careers.html#marketing-specialist"><monotone-shape shape-id=""
+                                    text="Marketing Specialist"></monotone-shape>Marketing Specialist</a></li>
+                        <li><a href="./careers.html#sales-lead"><monotone-shape shape-id=""
+                                    text="Sales Lead"></monotone-shape>Sales Lead</a></li>
+                        <li><a href="./careers.html#human-resources"><monotone-shape shape-id=""
+                                    text="Human Resources"></monotone-shape>Human Resources</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <span><a href="./plant-and-machinery.html">Plants & Machinery</a></span>
+                    <ul>
+                        <li><a href="./plant-and-machinery.html#tower-crane"><monotone-shape shape-id=""
+                                    text="Tower Crane"></monotone-shape>Tower Crane</a></li>
+                        <li><a href="./plant-and-machinery.html#jcb"><monotone-shape shape-id=""
+                                    text="JCB"></monotone-shape>JCB</a></li>
+                        <li><a href="./plant-and-machinery.html#dg-sets"><monotone-shape shape-id=""
+                                    text="DG Sets"></monotone-shape>DG Sets</a></li>
+                        <li><a href="./plant-and-machinery.html#batching-plant"><monotone-shape shape-id=""
+                                    text="Batching Plant"></monotone-shape>Batching Plant</a></li>
+                        <li><a href="./plant-and-machinery.html#concrete-pump"><monotone-shape shape-id=""
+                                    text="Concrete Pump"></monotone-shape>Concrete Pump</a></li>
+                        <li><a href="./plant-and-machinery.html"><monotone-shape shape-id=""
+                                    text="More"></monotone-shape>More</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <span><a href="./contact.html">Contact</a></span>
+                    <ul>
+                        <li><a href="tel:+919872000194" target="_blank"><monotone-shape shape-id=""
+                                    text="Phone"></monotone-shape>Phone</a></li>
+                        <li><a href="mailto:mail@dwcinfra.com" target="_blank"><monotone-shape shape-id=""
+                                    text="Email"></monotone-shape>Email</a></li>
+                        <li><a href="https://maps.app.goo.gl/ajSmBm9pyVc9ugWD9" target="_blank"><monotone-shape
+                                    shape-id="" text="Address"></monotone-shape>Address</a></li>
+                        <li><a href="https://www.dwcinfra.com"><monotone-shape shape-id=""
+                                    text="Website"></monotone-shape>Website</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <label>
+            <input type="checkbox" name="checkbox" aria-label="Toggle navbar">
+            <span></span>
+            <span></span>
+        </label>
+    </header>
+`;
+
+export class NavbarComponent extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        const nav = this.shadowRoot.querySelector("nav")
+        const lists = this.shadowRoot.querySelectorAll("nav>ul>li>ul")
+        const links = this.shadowRoot.querySelectorAll("nav > ul > li > span");
+        links.forEach(link => {
+            link.addEventListener("mouseover", () => {
+                links.forEach(l => l.classList?.remove("hover"));
+                link.classList.add("hover");
+                lists.forEach(list => {
+                    list.style.display = 'none';
+                    list.offsetHeight; // Force browser reflow
+                    list.style.display = '';
+                })
+            });
+        });
+        const checkbox = this.shadowRoot.querySelector("input[type='checkbox']");
+        checkbox.onclick = ()=>{
+            nav.style.display = 'none';
+            nav.offsetHeight; // Force browser reflow
+            nav.style.display = '';
+        }
+    }
+
+    disconnectedCallback() {
+        const links = this.shadowRoot.querySelectorAll("nav > ul > li > span");
+        links.forEach(link => {
+            link.removeEventListener("mouseover", this.handleMouseOver);
+        });
+    }
+}
+
+
+customElements.define('navbar-component', NavbarComponent);
