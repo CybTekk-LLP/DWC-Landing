e92b4a3187f987855d87eb215e8f1a60f28cacbe
yashrajbharti <y.bharti@cybtekk.com>
2024-11-08 13:33:27 +0530

    fix

--- js/video-modal-component.mjs
+++ js/video-modal-component.mjs
@@ -17,10 +17,10 @@ export class VideoModal extends HTMLElement {
             crossorigin
             playsinline
             muted
-            poster="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-HD.jpg"
+            poster="../images/poster.png"
           >
             <source
-              src="https://cdn.plyr.io/static/demo/View_From_A_Blue_Moon_Trailer-576p.mp4"
+              src="../videos/video.mp4"
               type="video/mp4"
             />
           </video>
